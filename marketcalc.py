b10837b11e8bc0aa0d975874de7d4d1545c24758
Solalunara <greenberg04@gmail.com>
2025-03-24 10:43:53 +0000

    drastic performance increase and returns graph

--- marketcalc.py
+++ marketcalc.py
@@ -22,47 +22,70 @@ def RandomPeriodYield( data, real_amnt_invested, monthly_contrib, months ):
 
 NUM_SIMS = 10000;
 INITIAL_VALUE = 0;
-MONTHLY_CONTRIB = 20000;
+MONTHLY_CONTRIB = 100000;
 SAMPLING_PERIOD = 12;
-MONTHS_SIMULATED_ARR = np.arange( 59 * 12, 60 * 12, 12 );
+MONTHS_SIMULATED_ARR = np.arange( 1 * 12, 60 * 12, 12 );
+
+data = pd.read_csv( 'ie_data.xls - Data.csv', quotechar='"' )
+
+MONTHS_SIMULATED_MAX = MONTHS_SIMULATED_ARR[ -1 ];
+sim_data = np.array( [ INITIAL_VALUE for _ in range( NUM_SIMS ) ], dtype=np.float64 );
+sim_data.shape = (1,NUM_SIMS);
+for i in range( int( MONTHS_SIMULATED_MAX / SAMPLING_PERIOD + 1 ) ):
+    new_array = sim_data[ -1 ] + RandomPeriodYield( data, sim_data[ -1 ], MONTHLY_CONTRIB, months=SAMPLING_PERIOD );
+    sim_data = np.vstack( (sim_data, new_array) );
+
+if ( MONTHLY_CONTRIB < 0 ):
+    MONTHLY_CONTRIB = 0;
 
 subzero_probabilities = [];
 subinit_probabilities = [];
-data = pd.read_csv( 'ie_data.xls - Data.csv', quotechar='"' )
+means = [];
+medians = [];
+percentiles = [];
 for MONTHS_SIMULATED in MONTHS_SIMULATED_ARR:
-    sim_data = np.array( [ INITIAL_VALUE for _ in range( NUM_SIMS ) ], dtype=np.float64 );
-    sim_data.shape = (1,NUM_SIMS);
-    for i in range( int( MONTHS_SIMULATED / SAMPLING_PERIOD ) ):
-        new_array = sim_data[ -1 ] + RandomPeriodYield( data, sim_data[ -1 ], MONTHLY_CONTRIB, months=SAMPLING_PERIOD );
-        sim_data = np.vstack( (sim_data, new_array) );
+    index = int( MONTHS_SIMULATED / SAMPLING_PERIOD );
+    means.append( np.mean( sim_data[ index ] ) );
+    medians.append( np.median( sim_data[ index ] ) );
 
-    print( f"mean: {np.mean( sim_data[ -1 ] )}" );
-    print( f"median: {np.median( sim_data[ -1 ] )}" );
+    percentiles.append( np.percentile( sim_data[ index ], np.arange( 0, 100, 1 ) ) );
 
-    if ( MONTHLY_CONTRIB < 0 ):
-        MONTHLY_CONTRIB = 0;
-
-    subzero = np.array( np.where( sim_data[ -1 ] <= 0 ) );
-    subzero_prob = subzero.size / sim_data.shape[ 1 ];
-    subinit = np.array( np.where( sim_data[ -1 ] <= INITIAL_VALUE + MONTHLY_CONTRIB * MONTHS_SIMULATED ) );
-    subinit_prob = subinit.size / sim_data.shape[ 1 ];
+    subzero = np.array( np.where( sim_data[ index ] < 0 ) );
+    subzero_prob = subzero.size / NUM_SIMS;
+    subinit = np.array( np.where( sim_data[ index ] < INITIAL_VALUE + MONTHLY_CONTRIB * MONTHS_SIMULATED ) );
+    subinit_prob = subinit.size / NUM_SIMS;
 
     subzero_probabilities.append( subzero_prob );
     subinit_probabilities.append( subinit_prob );
 
-    print( f"Hit zero probability: {subzero_prob*100}%" );
-    print( f"Sub initial value probability: {subinit_prob*100}%" );
-    
-    plt.hist( sim_data[ -1 ], bins=300, density=True );
-    plt.axvline( x=INITIAL_VALUE + MONTHLY_CONTRIB * MONTHS_SIMULATED, color='orange' );
-    plt.axvline( x=0, color='red' );
-    plt.axvline( x=np.mean( sim_data[ -1 ] ), color='blue' );
-    plt.axvline( x=np.median( sim_data[ -1 ] ), color='green' );
-    plt.show();
+    #plt.hist( sim_data[ index ], bins=300, density=True );
+    #plt.axvline( x=INITIAL_VALUE + MONTHLY_CONTRIB * MONTHS_SIMULATED, color='orange' );
+    #plt.axvline( x=0, color='red' );
+    #plt.axvline( x=np.mean( sim_data[ -1 ] ), color='blue' );
+    #plt.axvline( x=np.median( sim_data[ -1 ] ), color='green' );
+    #plt.show();
 
 subzero_probabilities = np.array( subzero_probabilities );
 subinit_probabilities = np.array( subinit_probabilities );
+percentiles = np.array( percentiles );
 
-plt.plot( MONTHS_SIMULATED_ARR, subzero_probabilities, color='red' );
-plt.plot( MONTHS_SIMULATED_ARR, subinit_probabilities, color='orange' );
+pb = plt.axes( (0.1, 0.1, 0.3, 0.8) );
+pb.set_title( 'Probabilities' );
+pb.plot( MONTHS_SIMULATED_ARR, subzero_probabilities, color='red', label='subzero' );
+pb.plot( MONTHS_SIMULATED_ARR, subinit_probabilities, color='orange', label='sub initial value' );
+pb.set_xlabel( "Months since start" );
+pb.set_ylabel( "Probability" );
+pb.legend();
+
+ret = plt.axes( (0.6, 0.1, 0.3, 0.8) );
+ret.set_title( 'Estimated Returns' );
+ret.plot( MONTHS_SIMULATED_ARR, means, color='red', label='Mean Value' );
+ret.plot( MONTHS_SIMULATED_ARR, medians, color='orange', label='Median Value' );
+ret.fill_between( MONTHS_SIMULATED_ARR, percentiles[ :, 5 ], percentiles[ :, 95 ], alpha=0.2, color='b', label='90% range' );
+ret.fill_between( MONTHS_SIMULATED_ARR, percentiles[ :, 25 ], percentiles[ :, 75 ], alpha=0.4, color='b', label='50% range' );
+ret.fill_between( MONTHS_SIMULATED_ARR, percentiles[ :, 40 ], percentiles[ :, 60 ], alpha=0.6, color='b', label='20% range' );
+ret.set_xlabel( "Months since start" );
+ret.set_ylabel( "Value" );
+ret.legend();
+
 plt.show();
